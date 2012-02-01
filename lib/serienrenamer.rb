7b724e7cd3bc5c1fe480cd9f976c22eff1967112
Philipp Böhm <philipp-boehm@live.de>
2012-02-01 23:49:27 +0100

    Tests und Episodenklasse so begonnen, dass mögliche Episoden am Namen erkannt werden

--- lib/serienrenamer.rb
+++ lib/serienrenamer.rb
@@ -2,5 +2,30 @@ $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 module Serienrenamer
-  VERSION = '0.0.1'
+    VERSION = '0.0.1'
+
+    class Episode
+
+        # patterns for suitable episodes  
+        @@patterns = [/S\d+E\d+/i, /\D+\d{3}\D+/]
+        
+        # create a episode if it could be 
+        # an episode
+        def initialize(episodepath)
+            @episodepath = episodepath 
+        end
+
+        def is_episode?
+
+            @@patterns.each do |p| 
+                if !@episodepath.match(p).nil?
+                    @suitable_pattern = p
+                    return true
+                end
+            end
+
+            return false
+        end
+    end
+
 end
--- test/test_serienrenamer.rb
+++ test/test_serienrenamer.rb
@@ -1,5 +1,6 @@
 require File.dirname(__FILE__) + '/test_helper.rb'
 
+
 class TestSerienrenamer < Test::Unit::TestCase
 
   def setup
@@ -8,4 +9,21 @@ class TestSerienrenamer < Test::Unit::TestCase
   def test_truth
     assert true
   end
+
+  def test_episode_creation
+      episode = Serienrenamer::Episode.new('Chuck')
+      assert_equal(true, episode.is_a?(Serienrenamer::Episode)) 
+  end
+
+  def test_episode_name_detection
+      episode = Serienrenamer::Episode.new('Chuck.S01E01.Dies.ist.ein.Test.German.Dubbed.avi')
+      assert_equal(true, episode.is_episode?) 
+
+      episode = Serienrenamer::Episode.new('chuck.512.hdtv-lol.avi')
+      assert_equal(true, episode.is_episode?) 
+
+      episode = Serienrenamer::Episode.new('video.flv')
+      assert_equal(false, episode.is_episode?) 
+  end
+
 end
