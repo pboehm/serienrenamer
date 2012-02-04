b5c809297192d938b7dd4f89fe4c204faeaec401
Philipp Böhm <philipp-boehm@live.de>
2012-02-04 20:35:09 +0100

    Added functionality for extracting information from the filename and build the wanted format out of tese information

--- lib/serienrenamer/episode.rb
+++ lib/serienrenamer/episode.rb
@@ -1,25 +1,46 @@
 class Serienrenamer::Episode
 
+    attr_reader :series, :season, :episode, :episodename
+
     # patterns for suitable episodes  
     @@patterns = [
+        # S01E01 ....
         /^(?<series>.*)S(?<season>\d+)E(?<episode>\d+)(?<episodename>.*)$/i, 
-        /^(?<series>.*)(?<season>\d{1,2})(?<episode>\d{2})(?<episodename>.*)$/, 
+        # 101  => S01E01;  1212 => S12E12
+        /^(?<series>.*\D)(?<season>\d+)(?<episode>\d{2})(?<episodename>\D.*)$/, 
+        # 1x1 => S01E01; 12x12 => S12E12
         /^(?<series>.*)(?<season>\d+)x(?<episode>\d+)(?<episodename>.*)$/, 
     ]
     
     # create a episode if it could be 
     # an episode
     def initialize(episodepath)
-        @episodepath = episodepath 
+        unless Serienrenamer::Episode.is_episode?(episodepath) then
+            raise ArgumentError, 'Not an episode'
+        end
+
+        # extract information from filename
+        pattern = @@patterns.select { |p| ! episodepath.match(p).nil? }[0]
+        raise ArgumentError, 'no suitable pattern found, no episode ?' unless pattern
+
+        infos = pattern.match(episodepath)
+        @series = Serienrenamer::Episode.clean_filename_info(infos[:series]).strip
+        @episodename = Serienrenamer::Episode.clean_filename_info(infos[:episodename]).strip
+        @season = infos[:season].to_i
+        @episode = infos[:episode].to_i
+
     end
 
-    def split_episode_parts
-        #parts = @suitable_pattern.match(@episodepath)
-        #puts "S%.2dE%.2d - %s" % [ parts[:season], parts[:episode], parts[:episodename] ]
+    def to_s
+        puts "S%.2dE%.2d - %s" % [ @season, @episode, @episodename ]
     end
 
     ###
     # static methods
+    def self.clean_filename_info(s)
+        s.gsub(/\./, " ")
+    end
+
     def self.is_episode?(episodefile)
         @@patterns.each do |p| 
             if episodefile.match(p) != nil then
--- test/test_serienrenamer.rb
+++ test/test_serienrenamer.rb
@@ -8,6 +8,7 @@ class TestSerienrenamer < Test::Unit::TestCase
         'chuck.512.hdtv-lol.avi',
         'chuck.1212.hdtv-lol.avi',
         'chuck.5x12.hdtv-lol.avi',
+        '5x12.avi',
     ]
 
     def test_episode_name_detection
@@ -17,5 +18,14 @@ class TestSerienrenamer < Test::Unit::TestCase
         }
 
         assert_equal(false, Serienrenamer::Episode.is_episode?('video.flv'))
+    end
+
+    def test_episode_information_extraction
+
+        assert_raise(ArgumentError) { Serienrenamer::Episode.new('video.flv')}
+       
+        assert_equal('Chuck', Serienrenamer::Episode.new(@@valid_filenames[0]).series)
+        assert_equal(1, Serienrenamer::Episode.new(@@valid_filenames[0]).season)
+        assert_equal(1, Serienrenamer::Episode.new(@@valid_filenames[0]).episode)
     end
 end
