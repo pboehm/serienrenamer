414788b5235b95a68ab57f333db8dc551f4db2c9
Philipp Böhm <philipp-boehm@live.de>
2012-02-06 21:16:59 +0100

    refactoring of Episode-Class so that it works with real Files instead of strings

--- lib/serienrenamer/episode.rb
+++ lib/serienrenamer/episode.rb
@@ -1,53 +1,140 @@
+require 'find'
+
 class Serienrenamer::Episode
 
-    attr_reader :series, :season, :episode, :episodename
+    attr_reader :series, :season, :episode, :episodename, :extension
+    attr_accessor :episodename_needed
 
     # patterns for suitable episodes  
-    @@patterns = [
-        # S01E01 ....
+    @@PATTERNS = [
+        # S01E01
         /^(?<series>.*)S(?<season>\d+)E(?<episode>\d+)(?<episodename>.*)$/i, 
-        # 101  => S01E01;  1212 => S12E12
+        # 101; 1212
         /^(?<series>.*\D)(?<season>\d+)(?<episode>\d{2})(?<episodename>\D.*)$/, 
-        # 1x1 => S01E01; 12x12 => S12E12
+        # 1x1; 12x12
         /^(?<series>.*)(?<season>\d+)x(?<episode>\d+)(?<episodename>.*)$/, 
     ]
-    
-    # create a episode if it could be 
-    # an episode
-    def initialize(episodepath)
-        unless Serienrenamer::Episode.is_episode?(episodepath) then
+   
+    # allowed endings for episode files
+    @@ENDINGS = %w( mpg mpeg avi mkv wmv mp4 mov flv 3gp ) 
+
+    # trash words that are removed from the episodename
+    @@TRASH_WORDS = %w( 
+        German Dubbed DVDRip HDTVRip XviD ITG TVR inspired HDRip
+        AMBiTiOUS RSG SiGHT SATRip WS TVS RiP READ GERMAN dTV aTV 
+        iNTERNAL CRoW MSE c0nFuSed UTOPiA scum EXPiRED BDRiP 
+        iTunesHD 720p x264 h264 CRiSP euHD WEBRiP ZZGtv ARCHiV 
+        Prim3time Nfo Repack SiMPTY BLURAYRiP BluRay DELiCiOUS 
+        UNDELiCiOUS fBi CiD iTunesHDRip RedSeven OiNK idTV 
+    )
+
+    # Constructor for the Episode-Class, which takes an episode as 
+    # argument and extracts as much as information from the file
+    # that it can.
+    def initialize(episodepath, episodename_needed=true)
+
+        unless File.exists?(episodepath) || Dir.exists?(episodepath)
+            raise ArgumentError, "episodepath not existing"
+        end
+        
+        basepath = File.basename(episodepath)
+
+        if File.file?(episodepath)
+            basepath = basepath.chomp(File.extname(basepath)) 
+        end
+
+        unless Serienrenamer::Episode.contains_episode_information?(basepath)
             raise ArgumentError, 'Not an episode'
         end
 
         # extract information from filename
-        pattern = @@patterns.select { |p| ! episodepath.match(p).nil? }[0]
+        pattern = @@PATTERNS.select { |p| ! basepath.match(p).nil? }[0]
         raise ArgumentError, 'no suitable pattern found, no episode ?' unless pattern
 
-        infos = pattern.match(episodepath)
-        @series = Serienrenamer::Episode.clean_filename_info(infos[:series]).strip
-        @episodename = Serienrenamer::Episode.clean_filename_info(infos[:episodename]).strip
+        infos = pattern.match(basepath)
+        raise ArgumentError, 'suitable pattern does not match the file' unless infos
+
+        @series = Serienrenamer::Episode.clean_episode_data(infos[:series]).strip
+        @episodename = Serienrenamer::Episode.clean_episode_data(infos[:episodename], true).strip
         @season = infos[:season].to_i
         @episode = infos[:episode].to_i
-
+        
+        ###
+        # setting up special behaviour
+        @episodename_needed=episodename_needed
+        @extension=File.extname(episodepath).gsub('.','')
     end
 
+    # Returns the episode information into a format like
+    # S0xE0x, depending on @episodename_needed it includes
+    # the episodename
     def to_s
-        puts "S%.2dE%.2d - %s" % [ @season, @episode, @episodename ]
+        if @episodename_needed    
+            return "S%.2dE%.2d - %s.%s" % [ @season, @episode, @episodename, @extension ]
+        else
+            return "S%.2dE%.2d.%s" % [ @season, @episode, @extension ]
+        end
     end
 
-    ###
-    # static methods
-    def self.clean_filename_info(s)
-        s.gsub(/\./, " ")
+    ##################
+    # static methods #
+    ##################
+
+    # cleans strings from things that can occur in
+    # episode files like dots (.) and trash words
+    def self.clean_episode_data(data, include_trashwords=false)
+        data.gsub!(/\./, " ")
+        
+        if include_trashwords
+            cleanwords = data.split(/ /).select do |w| 
+                @@TRASH_WORDS.grep(/^#{w}$/).empty? 
+            end
+            data = cleanwords.join(" ")
+        end
+
+        return data
     end
 
-    def self.is_episode?(episodefile)
-        @@patterns.each do |p| 
-            if episodefile.match(p) != nil then
+    # tries to match the given string against
+    # all supported regex-patterns and returns true if a 
+    # suitable regex is found
+    def self.contains_episode_information?(info)
+        @@PATTERNS.each do |p| 
+            if info.match(p) != nil 
                 return true
             end
         end
         return false
+    end
+
+    # tries to find a valid video file in a given path.
+    #
+    # If path is a file it returns path unchanged if file 
+    # is a valid video file or nil unless
+    #
+    # If path is a dir it searches for the biggest valid 
+    # videofile in it and returns the path or nil if nothing 
+    # found
+    def self.determine_video_file(path)
+        if File.file?(path)
+            matched_endings = @@ENDINGS.select { |e| ! path.match(/#{e}$/).nil? }
+            return path if ! matched_endings.empty?
+
+        elsif File.directory?(path)    
+            videofile = nil
+            for file in Find.find(path) do
+                matched_endings = @@ENDINGS.select { |e| ! file.match(/#{e}$/).nil? }
+                if ! matched_endings.empty? 
+                    if videofile == nil || File.size(file) > File.size(videofile) 
+                        videofile = file
+                    end
+                end
+            end
+
+            return videofile if videofile
+        end
+
+        return nil
     end
 end
 
--- test/test_episode.rb
+++ test/test_episode.rb
@@ -1,31 +1,50 @@
+# encoding: UTF-8
 require File.dirname(__FILE__) + '/test_helper.rb'
 require 'serienrenamer/episode.rb'
+require 'fileutils'
 
 class TestEpisode < Test::Unit::TestCase
 
     @@valid_filenames = [
-        'Chuck.S01E01.Dies.ist.ein.Test.German.Dubbed.avi',
-        'chuck.512.hdtv-lol.avi',
-        'chuck.1212.hdtv-lol.avi',
-        'chuck.5x12.hdtv-lol.avi',
-        '5x12.avi',
+        'test/testfiles/Chuck.S01E01.Dies.ist.ein.Test.German.Dubbed.BLURAYRiP.DELiCiOUS.WEBRiP.CiD.avi',
+        'test/testfiles/chuck.512.hdtv-lol.avi',
+        'test/testfiles/chuck.1212.hdtv-lol.avi',
+        'test/testfiles/chuck.5x12.hdtv-lol.avi',
+        'test/testfiles/5x12.avi',
     ]
+
+    def setup
+        @@valid_filenames.each { |f|
+            FileUtils.touch f unless File.file?(f)
+        }
+    end
 
     def test_episode_name_detection
  
         @@valid_filenames.each { |file|
-            assert_equal(true, Serienrenamer::Episode.is_episode?(file))
+            assert_equal(true, 
+                Serienrenamer::Episode.contains_episode_information?(file))
         }
 
-        assert_equal(false, Serienrenamer::Episode.is_episode?('video.flv'))
+        assert_equal(false,
+                Serienrenamer::Episode.contains_episode_information?('video.flv'))
     end
 
     def test_episode_information_extraction
 
         assert_raise(ArgumentError) { Serienrenamer::Episode.new('video.flv')}
-       
-        assert_equal('Chuck', Serienrenamer::Episode.new(@@valid_filenames[0]).series)
-        assert_equal(1, Serienrenamer::Episode.new(@@valid_filenames[0]).season)
-        assert_equal(1, Serienrenamer::Episode.new(@@valid_filenames[0]).episode)
+        
+        epi = Serienrenamer::Episode.new(@@valid_filenames[0]) 
+        assert_equal('Chuck',epi.series)
+        assert_equal(1, epi.season)
+        assert_equal(1, epi.episode)
+        assert_equal("Dies ist ein Test", epi.episodename)
+        assert_equal("S01E01 - Dies ist ein Test.avi", epi.to_s)
+    end
+
+    def test_videofile_determination
+        @@valid_filenames.each { |f| 
+            assert_not_nil(Serienrenamer::Episode.determine_video_file(f))
+        }    
     end
 end
