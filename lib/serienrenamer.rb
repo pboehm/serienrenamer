306f605dc62693828e77d9bf508b83782fe2e653
Philipp Böhm <philipp-boehm@live.de>
2012-03-19 23:31:18 +0100

    version 0.0.4

--- lib/serienrenamer.rb
+++ lib/serienrenamer.rb
@@ -3,7 +3,7 @@ $:.unshift(File.dirname(__FILE__)) unless
 
 
 module Serienrenamer
-    VERSION = '0.0.3'
+    VERSION = '0.0.4'
 
     require 'serienrenamer/episode.rb'
     require 'serienrenamer/information_store.rb'
